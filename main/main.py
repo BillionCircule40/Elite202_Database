a6e09640187b6a77a39af5b3d9e0c91ef7efc9f7
BillionCircule40 <64109992+BillionCircule40@users.noreply.github.com>
2024-04-05 18:52:19 -0500

    test of imprort of modules into main

--- main/main.py
+++ main/main.py
@@ -1,17 +1,4 @@
-#from  Account_create import account as AC
-
-class account:
-     
-     def __init__(self,name,pin):
-          account_info = {}
-          self.name= name
-          self.pin= pin
-          self.account_info = account_info
-          self.account_info.update({"name":self.name})
-          self.account_info.update({"pin":self.pin})
-     def __repr__(self):
-          return f'account name: {self.account_info.get("name")}\naccount pin: {self.account_info.get("pin")}' 
-     
+from modules import *
 
 
 
@@ -19,13 +6,11 @@ class account:
 def main():
      while True:
           print("welcome to the xxx bank online system\n-please log or create account to continue ")
-          print(account("ellie","01001"))
           x = int(input("0 login, 1 create"))
           if x ==0:
                print('log')
           else:
-               #AC()
-               print('log1')
+               print(ac.info())
 
 
 
