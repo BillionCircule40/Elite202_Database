a86f0a703d001133a92957c763e075ca2e76541f
BillionCircule40 <64109992+BillionCircule40@users.noreply.github.com>
2024-04-04 21:25:24 -0500

    worked class

--- main/main.py
+++ main/main.py
@@ -1,9 +1,17 @@
-import Account_create as AC
+#from  Account_create import account as AC
 
-
-
-
-
+class account:
+     
+     def __init__(self,name,pin):
+          account_info = {}
+          self.name= name
+          self.pin= pin
+          self.account_info = account_info
+          self.account_info.update({"name":self.name})
+          self.account_info.update({"pin":self.pin})
+     def __repr__(self):
+          return f'account name: {self.account_info.get("name")}\naccount pin: {self.account_info.get("pin")}' 
+     
 
 
 
@@ -11,12 +19,13 @@ import Account_create as AC
 def main():
      while True:
           print("welcome to the xxx bank online system\n-please log or create account to continue ")
+          print(account("ellie","01001"))
           x = int(input("0 login, 1 create"))
           if x ==0:
                print('log')
           else:
                #AC()
-               print(AC.account("ellie","01001"))
+               print('log1')
 
 
 
