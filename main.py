e53a7b45b107366062db3d2fcaf6f77095eb12b2
Ian <ianpomeroy@icloud.com>
2024-04-28 22:05:19 -0500

    fixed

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def create():
      ac_created = True
      ac_index.append(ac.info(root))
   
-     return_b = Button(root,text="return",command=main)
+     return_b = Button(root,text="return",command=lambda:main()& return_b.grid_forget())
      return_b.grid(row=19,column=0)
      wigit_index.append(return_b)
 
